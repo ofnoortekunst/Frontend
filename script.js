87f3e28158e638a55b74d68f6e57848f04c85e54
AinuRakk <lukasjaager@gmail.com>
2024-09-19 22:35:35 +0300

    Parandasin scale ja mugandasin JS

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const toggleButton = document.getElementById('toggle-btn');
 const sidebar = document.getElementById('sidebar');
 
-
+// Toggle the sidebar button.
 function toggleSidebar() {
   sidebar.classList.toggle('close');
   toggleButton.classList.toggle('rotate');
@@ -9,6 +9,22 @@ function toggleSidebar() {
   closeAllSubMenus();
 };
 
+// Automatically close and open the sidebar at a certain width.
+function checkWidth() {
+  if (window.innerWidth <= 1055) {
+    sidebar.classList.add('close');
+    toggleButton.classList.toggle('rotate');
+
+    closeAllSubMenus();
+  } else {
+    sidebar.classList.remove('close');
+    toggleButton.classList.toggle('rotate');
+  }
+}
+checkWidth();
+window.addEventListener('resize', checkWidth);
+
+// Toggle the submenus.
 function toggleSubMenu(button) {
 
   if (!button.nextElementSibling.classList.contains('show')) {
@@ -24,6 +40,7 @@ function toggleSubMenu(button) {
   };
 };
 
+// Close all submenus.
 function closeAllSubMenus() {
   Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
     ul.classList.remove('show');
@@ -31,6 +48,7 @@ function closeAllSubMenus() {
   });
 };
 
+// Change the 'favourite' icon based on if the navbar has gone to the side.
 document.getElementById('sidebar-active').addEventListener('change', function() {
   const changeColorIcon = document.querySelector('.icon');
 
