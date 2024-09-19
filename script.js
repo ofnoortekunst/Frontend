507a50dd61f034e693c0b3e922f229d94f914c12
AinuRakk <lukasjaager@gmail.com>
2024-09-19 21:29:51 +0300

    First HTML, CSS and JS commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const toggleButton = document.getElementById('toggle-btn');
+const sidebar = document.getElementById('sidebar');
+
+
+function toggleSidebar() {
+  sidebar.classList.toggle('close');
+  toggleButton.classList.toggle('rotate');
+
+  closeAllSubMenus();
+};
+
+function toggleSubMenu(button) {
+
+  if (!button.nextElementSibling.classList.contains('show')) {
+    closeAllSubMenus();
+  };
+
+  button.nextElementSibling.classList.toggle('show');
+  button.classList.toggle('rotate');
+
+  if (sidebar.classList.contains('close')) {
+    sidebar.classList.toggle('close');
+    toggleButton.classList.toggle('rotate');
+  };
+};
+
+function closeAllSubMenus() {
+  Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
+    ul.classList.remove('show');
+    ul.previousElementSibling.classList.remove('rotate');
+  });
+};
+
+document.getElementById("sidebar-active").addEventListener('change', function() {
+  const changeColorIcon = document.querySelector('.icon');
+  
+  if (isChecked) {
+    changeColorIcon.innerHTML = '<img alt="bookmark" src="images/bookmark-color-change.png" class="bookmark">Lemmikud';
+  };
+});
+
+document.getElementById('sidebar-active').addEventListener('change', function() {
+  const changeColorIcon = document.querySelector('.icon');
+
+  if (this.checked) {
+    changeColorIcon.innerHTML = '<img alt="bookmark" src="images/bookmark-color-change.png" class="bookmark">Lemmikud';
+  } else {
+    changeColorIcon.innerHTML = '<img alt="bookmark" src="images/pro-bookmark.png" class="bookmark">Lemmikud';
+  }
+});
