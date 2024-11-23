f4feae38513f5fa828e690414606abf1d3814d11
proovend <proovendone@proton.me>
2024-11-23 11:09:21 +0200

    add cwd

--- pmconfig.js
+++ pmconfig.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'noortekunst-dev', // App name
       script: 'node_modules/.bin/next', // Path to the Next.js binary
       args: 'start', // Run in "start" mode for production
-      cwd: './', // Current working directory
+      cwd: '/data02/virt133817/website', // Current working directory
       watch: true,
       env: {
         NODE_ENV: 'development', // Set environment to development
