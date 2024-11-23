53c362747e12569a62e8216e8def3049b0082ff4
proovend <proovendone@proton.me>
2024-11-23 19:08:25 +0200

    fix config.js references

--- next.config.js
+++ next.config.js
@@ -3,51 +3,51 @@ rewrites: async () => {
   return [
     {
       source: "/",
-      destination: "/home_page/index",
+      destination: "/home_page/index.html",
     },
     {
       source: "/acc_page_interested",
-      destination: "/acc_interested/acc_page_interested",
+      destination: "/acc_interested/acc_page_interested.html",
     },
     {
       source: "/acc_artist",
-      destination: "/acc_artist/acc_page_artist",
+      destination: "/acc_artist/acc_page_artist.html",
     },
     {
       source: "/artist_page",
-      destination: "/artist-page/artist_page",
+      destination: "/artist-page/artist_page.html",
     },
     {
       source: "/artist_works",
-      destination: "/artist_works/artist_works",
+      destination: "/artist_works/artist_works.html",
     },
     {
       source: "/favourites_page",
-      destination: "/favourites_page/favourites_page",
+      destination: "/favourites_page/favourites_page.html",
     },
     {
       source: "/help_page",
-      destination: "/help_page/help_page",
+      destination: "/help_page/help_page.html",
     },
     {
       source: "/index",
-      destination: "/home_page/index",
+      destination: "/home_page/index.html",
     },
     {
       source: "/login_register_page",
-      destination: "/login/login_register_page",
+      destination: "/login/login_register_page.html",
     },
     {
       source: "/upload_work",
-      destination: "/upload_work/upload_work",
+      destination: "/upload_work/upload_work.html",
     },
     {
       source: "/work_opened",
-      destination: "/work_opened/work_opened",
+      destination: "/work_opened/work_opened.html",
     },
     {
       source: "/works",
-      destination: "/works_page/works",
+      destination: "/works_page/works.html",
     },
   ]
 },
