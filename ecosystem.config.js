76b9c0f2de494b16314a1eea5375076d50631fc7
proovend <proovendone@proton.me>
2024-11-23 14:11:35 +0200

    nvm

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'noortekunst-dev', // App name
-      script: 'node_modules/next/dist/bin/next', // Path to the Next.js binary
+      script: 'npm', // Path to the Next.js binary
       args: 'run dev', // Run in "start" mode for production
       cwd: '/data02/virt133817/website', // Current working directory
       watch: true,
