b81d695f1cac7164627f06a3e62b728957da257a
AinuRakk <lukasjaager@gmail.com>
2024-11-27 00:32:59 +0200

    checkbox muudatused

--- public/works_page/script.js
+++ public/works_page/script.js
@@ -222,3 +222,36 @@ themeSwitch.addEventListener('click', () => {
 function openPage(pageUrl){
   window.location.href = pageUrl;
 }
+
+// Price scale
+function getVals() {
+  // Get slider values
+  let parent = this.parentNode;
+  let slides = parent.querySelectorAll(".range-input");
+  let slide1 = parseFloat(slides[0].value);
+  let slide2 = parseFloat(slides[1].value);
+
+  // Cross Range 
+  if (slide1 > slide2) {
+    let tmp = slide2;
+    slide2 = slide1;
+    slide1 = tmp;
+  }
+
+  let displayElement = document.getElementById("range-values");
+  displayElement.innerHTML = "$" + slide1 + " &nbsp-&nbsp $" + slide2;
+};
+
+window.onload = function () {
+  let sliderSections = document.getElementById("range-slider");
+  let sliders = sliderSections.querySelectorAll(".range-input");
+
+  sliders.forEach((slide) => {
+    if (slide.type === "range") {
+      // Change Value Method
+      slide.oninput = getVals;
+      // Initial Value
+      slide.oninput();
+    }
+  });
+};
